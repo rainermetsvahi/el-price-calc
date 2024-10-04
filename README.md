bcdcefd1eaecea0fe9e7878b1b25f44b17eec4dc
Rainer Metsvahi <rainer@cyber.ee>
2024-10-04 07:07:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Script to calculate electricity bill based on consumption and electricity prices.
+
+Expects input from .csv file imported from Elering (https://estfeed.elering.ee/metering-data-consumption)
+
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufReader, Lines, Result};
+
+fn read_lines(file_path: &str) -> Result<Lines<BufReader<File>>> {
+    let file = File::open(file_path)?;
+    Ok(BufReader::new(file).lines())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    println!("Reading file {file_path}");
+
+    match read_lines(file_path) {
+        Ok(lines) => {
+            for line in lines {
+                if let Ok(ip) = line {
+                    println!("{}", ip);
+                }
+            }
+        }
+        Err(e) => println!("Error reading file: {}", e),
+    }
+}
